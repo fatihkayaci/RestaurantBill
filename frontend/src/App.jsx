a8707aee6cbfa1b46c0a05fadab82adc3c8c885b
Fatih Kayacı <fatihkayaci@yahoo.com>
2026-01-19 12:53:31 +0300

    feat: frontend file created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button, Card, Result } from 'antd'; // Ant Design'dan parçaları çağırdık
+
+function App() {
+  return (
+    <div style={{ padding: '50px' }}>
+    </div>
+  );
+}
+
+export default App;
