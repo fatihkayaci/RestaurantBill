b94234e0478e6ad6794898454de55791787ecf4f
Fatih Kayacı <fatihkayaci@yahoo.com>
2026-02-14 18:59:42 +0300

    feat: Empty interface created for Repositories.

---
+++ Backend/RestaurantBill.Domain/Interfaces/IOrederItemRepository.cs
@@ -0,0 +1,9 @@
+using RestaurantBill.Domain.Entities;
+
+namespace RestaurantBill.Domain.Interfaces;
+
+
+public interface IOrderItemRepository : IGenericRepository<OrderItem>
+{
+    // to be filled
+} 
