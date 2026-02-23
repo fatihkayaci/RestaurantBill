1dcf10ece358509c9aa706e57f087fe613ea6592
Fatih Kayacı <fatihkayaci@yahoo.com>
2026-02-24 00:14:11 +0300

    feat: guard class fixed.

--- Backend/RestaurantBill.Application/Common/Guard.cs
+++ Backend/RestaurantBill.Application/Common/Guard.cs
@@ -1,10 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using RestaurantBill.Application.Exceptions;
 
 namespace RestaurantBill.Application.Common;
 
 public static class Guard
 {
-    public static void AgainstNull(object entity, string message = "Kayıt bulunamadı.")
+    public static void AgainstNull<T>([NotNull] T? entity, string message = "Kayıt bulunamadı.") where T : class
     {
         if (entity == null)
             throw new NotFoundException(message);
