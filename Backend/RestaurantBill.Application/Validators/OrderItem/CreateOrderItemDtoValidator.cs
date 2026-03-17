4d8dbf4313e4c5fc3f85eb24e33412a8f5099f2a
Fatih Kayacı <fatihkayaci@yahoo.com>
2026-03-17 18:07:22 +0300

    fixed: Validation error fixed to orderItem

--- Backend/RestaurantBill.Application/Validators/OrderItem/CreateOrderItemDtoValidator.cs
+++ Backend/RestaurantBill.Application/Validators/OrderItem/CreateOrderItemDtoValidator.cs
@@ -7,8 +7,8 @@ public class CreateOrderItemDtoValidator : AbstractValidator<CreateOrderItemDto>
 {
     public CreateOrderItemDtoValidator()
     {
-        RuleFor(x => x.OrderId)
-            .GreaterThan(0).WithMessage("Geçersiz bir sipariş seçtiniz.");
+        // RuleFor(x => x.OrderId)
+        //     .GreaterThan(0).WithMessage("Geçersiz bir sipariş seçtiniz.");
         RuleFor(x => x.ProductId)
             .GreaterThan(0).WithMessage("Geçersiz bir ürün seçtiniz.");
         RuleFor(x => x.Quantity)
